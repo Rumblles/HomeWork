4197e44ef1784bf9004e53b0612587f00899d888
Rumblles <rustikbogdanov19@gmail.com>
2024-09-13 13:53:26 +0300

    Update module_1_4.py

--- module_1_4.py
+++ module_1_4.py
@@ -1,6 +1,7 @@
 my_string = input('Назовите Вашего питомца: ')
 print(my_string.upper())
 print(my_string.lower())
-print(my_string.replace(' ','#'))
+print(len(my_string))
+print(my_string.replace(' ',''))
 print(my_string[0])
 print(my_string[-1])
