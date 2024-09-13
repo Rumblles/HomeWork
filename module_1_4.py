8faf498146ed8d7a00078d7e899d0598f708a90d
Rumblles <rustikbogdanov19@gmail.com>
2024-09-13 17:35:35 +0300

    Update module_1_4.py

--- module_1_4.py
+++ module_1_4.py
@@ -5,3 +5,8 @@ print(len(my_string))
 print(my_string.replace(' ',''))
 print(my_string[0])
 print(my_string[-1])
+first_name = 'Гриша'
+sock_count = 11
+second_name = 'Валере'
+match_count = 7
+print(first_name,'дал',second_name ,sock_count,'носков и ', match_count,'спичек')
