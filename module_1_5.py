33620b9826f4678a202f25069cd3d08136664464
Rumblles <rustikbogdanov19@gmail.com>
2024-09-13 18:53:55 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,6 @@
+immutable_var = (1,23,47,'pain',True)
+print(immutable_var)
+#immutable_var_[0] = 11 #кортеж неизменяемая коллекция, с возможностью встроить изменяемые элементы внесённые в квадратные скобки []
+mutable_list = ['cat','dog','pig']
+mutable_list[1] = 'rat'
+print(mutable_list)
