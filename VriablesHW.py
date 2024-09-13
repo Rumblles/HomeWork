1128fcf52d05abb642f35fb5c8f881f64a2c49d7
Rumblles <rustikbogdanov19@gmail.com>
2024-09-13 12:28:56 +0300

    Update VriablesHW.py

--- VriablesHW.py
+++ VriablesHW.py
@@ -1,5 +1,6 @@
 completed_tasks = 12
 hours_spent = 1.5
 developer = 'python'
-time_for_one_task = (1.5/12)
+print(hours_spent/comleted_tasks)
+time_for_one_task = (hours_spent/completed_tasks)
 print(developer, completed_tasks, hours_spent, time_for_one_task)
